7b2303b13f1da32ae22e648ad146f7240b971806
Vince Veselosky <vince@control-escape.com>
2025-05-31 18:57:25 -0400

    Add copyright notice to Python files

--- src/django_hugo/__init__.py
+++ src/django_hugo/__init__.py
@@ -1,2 +1,15 @@
-def hello() -> str:
-    return "Hello from django-hugo!"
+# AGPL Notice: This file is part of django-hugo.
+# Copyright (C) 2025 Vincent Veselosky
+#
+# This package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <https://www.gnu.org/licenses/>.
--- testproject/urls.py
+++ testproject/urls.py
@@ -1,3 +1,18 @@
+# AGPL Notice: This file is part of django-hugo.
+# Copyright (C) 2025 Vincent Veselosky
+#
+# This package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <https://www.gnu.org/licenses/>.
 """
 URL configuration for testproject project.
 
@@ -14,9 +29,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]

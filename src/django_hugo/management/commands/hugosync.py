2d4cf80870e5aa3a67a90bd6ca2b1890da648e4a
Vince Veselosky <vince@control-escape.com>
2025-06-09 19:29:38 -0400

    Add copyright notice to mgmt command

--- src/django_hugo/management/commands/hugosync.py
+++ src/django_hugo/management/commands/hugosync.py
@@ -1,3 +1,18 @@
+# AGPL Notice: This file is part of django-hugo.
+# Copyright (C) 2025 Vincent Veselosky
+#
+# This package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <https://www.gnu.org/licenses/>.
 from django.apps import apps
 from django.core.management.base import BaseCommand
 
